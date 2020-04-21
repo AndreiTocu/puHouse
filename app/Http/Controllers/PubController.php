566e542f93460b504525aa439f9fb05a7f270665
Andrei Tocu <tocuandrei39@gmail.com>
2020-04-21 18:11:56 +0300

    PubController modified

--- app/Http/Controllers/PubController.php
+++ app/Http/Controllers/PubController.php
@@ -3,14 +3,23 @@
 namespace App\Http\Controllers;
 
 use \App\Pub;
+use \App\Review;
 use Illuminate\Http\Request;
 
 class PubController extends Controller
 {
-    public function show() {
+    public function lists() {
 
       $pubs = Pub::all();
 
-      return view('Pubs.show', compact('pubs'));
+      return view('Pubs.lists', compact('pubs'));
+    }
+
+    public function showPub($pubProfileId) {
+
+      $pubProfile = Pub::find($pubProfileId);
+      $pubReviews = Review::all()->where('pub_id', '=', $pubProfileId) -> toArray();
+      
+      return view('Pubs.pub-profile', compact('pubProfile', 'pubReviews'));
     }
 }
