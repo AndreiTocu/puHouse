dc1824b77ad50cc95d27c6f1d7248d3743da386f
Andrei Tocu <tocuandrei39@gmail.com>
2020-04-21 18:49:32 +0300

    app.js

--- public/js/app.js
+++ public/js/app.js
@@ -37393,8 +37393,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\ASUS\puHouse_Project\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\ASUS\puHouse_Project\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\ASUS\puHouse_Project\puHouse\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\ASUS\puHouse_Project\puHouse\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
