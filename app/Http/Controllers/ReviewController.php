63820a850c8ca69b5e903f8c6ca7aa56c0e28bb5
Andrei Tocu <tocuandrei39@gmail.com>
2020-05-02 18:37:49 +0300

    Stores the reviews into the database

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -9,10 +9,10 @@ use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    public function store(Request $request) {
+  public function store(Request $request) {
 
-      Review::create($request->all() + ['user_id' => auth()->id()]);
+    Review::create($request->all() + ['user_id' => auth()->id()]);
 
-      return  redirect()->back();
-    }
+    return  redirect()->back();
+  }
 }
