3307189de78fb5a3c07183e9fa7c6ced301d17b3
Andrei Tocu <tocuandrei39@gmail.com>
2020-05-03 16:51:09 +0300

    Pubs list view

--- resources/views/Pubs/lists.blade.php
+++ resources/views/Pubs/lists.blade.php
@@ -1,42 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-8">
-          <div class="card-header">
-              <h1>
-                <strong>PUBS</strong>
 
   <!-- Style for the Login feature -->
   <link href="{{ asset('css/Login.css') }}" rel="stylesheet">
 
+  <div class="container pt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-12">
+        
+        <div class="card card-signin my-5">
+        @foreach ($pubs as $pub)
+          <!-- Pub profile -->
+            <div class="card-body">
+              <h1 class="my-4"> <strong> {{ $pub -> name }} </strong>
+                  <small></small>
               </h1>
-          </div>
-          @foreach ($pubs as $pub)
-            <div class="card mt-4">
-              <div class="card-header">
-                <a href="/Pubs/{{ $pub->id }}"> <strong> {{ $pub -> name }} </strong> </a>
+              <div class="row p-4">
+                <div class="col-md-7">
+                  <a href="/Pubs/{{ $pub->id }}">
+                    <img class="img-fluid rounded mb-3 mb-md-0" src="http://placehold.it/700x300" alt="">
+                  </a>
+                </div>
+                <div class="col-md-5 text-center">
+                  <h2 class="font-italic" >Schedule</h2>
+                  <h4>{{ $pub->OpeningTime }} - {{ $pub->ClosingTime }}</h4>
+                  <br>
+                  <h2 class="font-italic" >Address</h2>
+                  <h4>{{ $pub->Adress }}</h4>
+                </div>
               </div>
-              <div class="card-body">
-                <ul class="list-group">
-                  <li class="list-group-item">
-                    {{ $pub -> OpeningTime }} - {{ $pub -> ClosingTime }}
-                    <span class="badge badge-dark badge-pill">Schedule</span>
-                  </li>
-                  <li class="list-group-item">
-                    {{ $pub -> Adress }}
-                    <span class="badge badge-dark badge-pill">Adress</span>
-                  </li>
-                  <li class="list-group-item">
-                    {{ $pub -> Description }}
-                    <span class="badge badge-dark badge-pill">Description</span>
-                  </li>
-                </ul>
-              </div>
+              <a  class="btn btn-lg btn-primary btn-block text-uppercase rounded-pill" role="button" href="/Pubs/{{ $pub->id }}">View {{ $pub -> name }}</a>
+              <hr class="my-4">
             </div>
           @endforeach
           </div>
-        </div>
       </div>
+    </div>
+  </div>
 @endsection
