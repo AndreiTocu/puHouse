9a51e2da1e5c4fa72dab1585532417e2ebb558c0
Andrei Tocu <tocuandrei39@gmail.com>
2020-05-03 16:29:42 +0300

    Updates user data and returns to the update view with a message.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,4 +14,27 @@ class UserController extends Controller
 
       return view('Users.user-profile', compact('user'));
     }
+
+    public function update(Request $request) {
+
+      $user = Auth::user();
+
+      $NewData = $request->validate([
+        'name' => 'required',
+        'email' => 'required|email'
+      ]);
+
+      //A condition that helps to modify the data only if there are changes
+      if(strcmp($user['name'], $NewData['name']) != 0 || strcmp($user['email'], $NewData['email']) != 0) {
+
+        $user['name'] = $NewData['name'];
+        $user['email'] = $NewData['email'];
+
+        $user->save();
+
+        return redirect()->back()->with('message', 'Changed Successfully!');
+      } else {
+          return redirect()->back();
+      }
+    }
 }
