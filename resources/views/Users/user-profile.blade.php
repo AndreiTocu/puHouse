0743a8de154bc10345c4c023366634194883ab0b
Andrei Tocu <tocuandrei39@gmail.com>
2020-05-03 17:00:56 +0300

    User profile view

---
+++ resources/views/Users/user-profile.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <!-- Register css file -->
+  <link rel="stylesheet" href="/css/Register.css">
+
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-lg-10 col-xl-9 mx-auto">
+        <div class="card card-signin flex-row my-5">
+            <div class="card-body">
+              <div class="row">
+
+                <div class="col-lg-6 text-center">
+                  <img class="rounded-circle" src="images-users/{{ Auth::user()->avatar }}"
+                                              alt="Generic placeholder image" width="150" height="150">
+                  <h5 class="card-title text-center pt-3">{{ $user->name }} - Profile</h5>
+                </div>
+
+                <div class="col-lg-6 text-center">
+                  <div class="container text-center">
+                    <div class="card-heder">
+                       <span class="badge badge-dark badge-pill">Name</span>
+                    </div>
+                    <div class="card-body">
+                      {{ $user->name }}
+                    </div>
+                  </div>
+
+                  <div class="container text-center">
+                    <div class="card-heder">
+                       <span class="badge badge-dark badge-pill">E-mail</span>
+                     </div>
+                    <div class="card-body">
+                      {{ $user->email }}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- Row -->
+              <hr>
+              <div class="row pt-3">
+                <div class="col-lg-6 text-center">
+                  <a class="btn btn-primary" href="{{ route('UserProfileUpdate') }}">Edit Profile</a>
+                </div>
+                <div class="col-lg-6 text-center">
+                  <a class="btn btn-danger" href="{{ route('UserDelete') }}">Delete Profile</a>
+                </div>
+              </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
