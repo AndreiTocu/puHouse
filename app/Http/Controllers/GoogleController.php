1444b70c43b03558ca2124985ef4bf4965471e07
Andrei Tocu <tocuandrei39@gmail.com>
2020-05-12 14:49:19 +0300

    The Google Controller

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Socialite;
+use Auth;
+use Exception;
+use App\User;
+
+class GoogleController extends Controller
+{
+}
