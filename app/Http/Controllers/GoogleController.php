8aa3e5610a8ea4f311538a1851414a8e5c8dbd7e
Andrei Tocu <tocuandrei39@gmail.com>
2020-05-12 14:50:28 +0300

    The Callback function

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -20,4 +20,40 @@ class GoogleController extends Controller
         return Socialite::driver('google')->redirect();
     }
 
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function handleGoogleCallback()
+    {
+        try {
+
+            $user = Socialite::driver('google')->user();
+
+            $finduser = User::where('google_id', $user->id)->first();
+
+            if($finduser){
+
+                Auth::login($finduser);
+
+                return redirect('/');
+
+            }else{
+                $newUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'google_id'=> $user->id,
+                    'password' => encrypt('123456dummy')
+                ]);
+
+                Auth::login($newUser);
+
+                return redirect('/');
+            }
+
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+    }
 }
