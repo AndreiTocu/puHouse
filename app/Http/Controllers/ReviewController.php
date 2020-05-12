0cce68ec227d5b9a964f0a3eba412dc2cbd3c9fa
Andrei Tocu <tocuandrei39@gmail.com>
2020-05-12 14:52:52 +0300

    The delete-review feature.

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -6,6 +6,7 @@ use \App\Pub;
 use \App\Review;
 use \App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReviewController extends Controller
 {
@@ -14,5 +15,14 @@ class ReviewController extends Controller
     Review::create($request->all() + ['user_id' => auth()->id()]);
 
     return  redirect()->back();
+  }
+
+  public function delete($reviewId) {
+
+    $dataDel = Review::find($reviewId);
+
+    $dataDel -> delete();
+
+    return redirect()->back();
   }
 }
